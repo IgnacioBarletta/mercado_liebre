3a21d1535c3180d0774b7c32f648fb39474e58d4
Ignacio Barletta Trípodi <IgnacioBarletta@users.noreply.github.com>
2021-05-18 21:55:32 -0300

    agrego procfile y modificio app

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
-
 const publicpath = path.resolve(__dirname, './public');
+
 app.use(express.static(publicpath));
 
-app.listen(3000, () => {
+app.listen((prcess.env.PORT || 3000, () => {
     console.log('Servidor corriendo en el puerto 3000')
 });
 
